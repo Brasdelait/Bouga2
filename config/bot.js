ff7463f4d5d1e8c358027143fe50a6b451998c51
Brasdelait <87140201+Brasdelait@users.noreply.github.com>
2021-07-11 13:55:30 +0200

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODYzMDMzNDczNjA5NDk4NjU0.YOhAjg.8iZNLP4GpevePLHCwi2uIb_dooE',
+        prefix: ';',
+        activity: '',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
